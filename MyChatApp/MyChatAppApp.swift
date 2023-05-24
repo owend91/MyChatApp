449a1a99ec1da651ba4355c7a355fdf20e677dda
David Owen <owend91@gmail.com>
2023-05-24 10:25:43 -0600

    Initial Commit

---
+++ MyChatApp/MyChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatAppApp.swift
+//  MyChatApp
+//
+//  Created by David Owen on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MyChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
